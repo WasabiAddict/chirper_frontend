b4bbbe3cc998c93c540268ae47db7bbdb6688f18
WasabiAddict <davidtltran@gmail.com>
2022-03-18 09:29:21 -0500

    not pretty

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [username, setUsername] = useState("");
+  const [thoughts, setThoughts] = useState("");
+  const [print, setPrint] = useState(false);
+  const form = document.querySelector("form");
+  const API_URL = "http://localhost:3000/mew";
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+  const handleThoughtsChange = (e) => {
+    setThoughts(e.target.value);
+  };
+  const handleButtonClick = (e) => {
+    //setPrint(true);
+    e.preventDefault();
+    const formData = new FormData(form);
+    const name = formData.get("name");
+    const content = formData.get("content");
+
+    const mew = {
+      name,
+      content,
+    };
+    
+  
+      const div = document.createElement("div");
+      div.id = 'newDiv';
+      
+
+      const header = document.createElement("h3");
+      header.textContent = name;
+
+      const contents = document.createElement("p");
+      contents.textContent = content;
+
+      const date = document.createElement('small');
+      date.textContent = new Date();
+
+      div.appendChild(header);
+      div.appendChild(contents);
+      div.appendChild(date);
+
+      document.body.appendChild(div);
+      setThoughts('');
+      setUsername('');
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form
+        className="container "
+        autoComplete="on"
+      >
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          name="name"
+          onChange={handleUsernameChange}
+        />
+        <textarea
+          type="text"
+          placeholder="Any thoughts?"
+          value={thoughts}
+          name="content"
+          onChange={handleThoughtsChange}
+        />
+        <button className="btn" onClick={handleButtonClick}>
+          Chirp it
+        </button>
+      </form>
+      
+    </>
   );
-}
+};
 
 export default App;
